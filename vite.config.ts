8852232b5484e0f9f268e580c0a732ac384bc14e
gtut_Betty <gtut3252300@gmail.com>
2025-11-25 21:55:47 +0800

    修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'site-map',
   plugins: [vue(), basicSsl()],
   resolve: {
     alias: {
